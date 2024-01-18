f1f1f5bd5ee4e98e8383c9953f657aabe48d8d76
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-01-18 21:23:02 +0530

    Add mock note data to noteMocks array

--- src/renderer/src/store/mocks/index.ts
+++ src/renderer/src/store/mocks/index.ts
@@ -1,3 +1,20 @@
 import type { NoteInfo } from "@shared/models";
 
-export const noteMocks: NoteInfo[] = [];
+export const noteMocks: NoteInfo[] = [
+  {
+    title: "Welcome 👋",
+    lastEdited: new Date().getTime(),
+  },
+  {
+    title: "Note 1",
+    lastEdited: new Date().getTime(),
+  },
+  {
+    title: "Note 2",
+    lastEdited: new Date().getTime(),
+  },
+  {
+    title: "Note 3",
+    lastEdited: new Date().getTime(),
+  },
+];
